785bacb939ad919deae6ccbd75020655ec7b6f11
Ivan Fernandez <100383564@alumnos.uc3m.es>
2020-12-17 19:00:37 +0100

    Edit button is now functional

--- js/course.js
+++ js/course.js
@@ -1,3 +1,4 @@
+var editing;
 $(document).ready(function() {
     //First show course
     changeCentral("#course");
@@ -19,10 +20,25 @@ $(document).ready(function() {
         changeCentral("#groups");
         showForum();
     });
+    $(".grades_button").click(function() {
+        changeCentral("#grades");
+        showForum();
+    });
 });
 
 //Function to change central content
 function changeCentral(toChange) {
     $(".central").hide();
     $(toChange).show();
+}
+
+function edit() {
+    var select = $('#edit-select').val();
+    var title = $('#edit-title').val();
+    var markup = `<div class="item-container">
+    <img class="icon" src="../images/${select}">
+    <a href="#">${title}</a>
+</div>`
+    $(`#${editing}`).append(markup);
+    $(".edit-popup").hide();
 }
