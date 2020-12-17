cbd767716cc304f4a786f6160ed0cf4ddfe75150
Ivan Fernandez <100383564@alumnos.uc3m.es>
2020-12-17 17:46:22 +0100

    Changes to central container courses

---
+++ js/course.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    //First show course
+    changeCentral("#course");
+
+    //Changeing of central container
+    $(".course_button").click(function() {
+        changeCentral("#course");
+        showForum();
+    });
+    $(".participants_button").click(function() {
+        changeCentral("#participants");
+        showForum();
+    });
+    $(".forum_button").click(function() {
+        changeCentral("#forum");
+        showForum();
+    });
+});
+
+//Function to change central content
+function changeCentral(toChange) {
+    $(".central").hide();
+    $(toChange).show();
+}
