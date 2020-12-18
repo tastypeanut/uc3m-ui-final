23363151f7c16707db1e841da7a952588cf466f8
Ivan Fernandez <100383564@alumnos.uc3m.es>
2020-12-18 16:35:38 +0100

    Admin updates

--- js/main-admin.js
+++ js/main-admin.js
@@ -1,28 +1,24 @@
-var cookie_in_use = localStorage.getItem('cookie_in_use');
-if (cookie_in_use == null || getCookie(cookie_in_use) == null || JSON.parse(atob(getCookie(cookie_in_use)))['role'] != "Administrator") {
-    window.location.href = "../auth.html";
-}
+// var cookie_in_use = localStorage.getItem('cookie_in_use');
+// if (cookie_in_use == null || getCookie(cookie_in_use) == null || JSON.parse(atob(getCookie(cookie_in_use)))['role'] != "Administrator") {
+//     window.location.href = "../auth.html";
+// }
 
-$(document).ready(function(){
+$(document).ready(function() {
     //First show course
     changeCentral("#events");
-    
+
     //Changeing of central container
     $(".buttonHome").click(function() {
         changeCentral("#events");
-        showForum();
     });
     $(".students_button").click(function() {
         changeCentral("#students");
-        showForum();
     });
     $(".teachers_button").click(function() {
         changeCentral("#teachers");
-        showForum();
     });
     $(".courses_button").click(function() {
         changeCentral("#courses");
-        showForum();
     });
 });
 
@@ -30,4 +26,20 @@ $(document).ready(function(){
 function changeCentral(toChange) {
     $(".central").hide();
     $(toChange).show();
+}
+
+function createCourse() {
+    var title = $('#edit-title').val();
+    var prof = $('#edit-proffessor').val();
+    var markup = `<article class='course-card'>
+    <h4 class="course-name">${title}</h4>
+    <div class="course-info">
+        <h5 class="professor" style="flex-grow: 1;">Professor: ${prof}</h5>
+        <a class="buttonEdit" href="#" onclick="$( '.add-teacher-popup').show();">+ teacher</a>
+        <a class="buttonEdit" href="#" onclick="$( '.add-student-popup').show();">+ student</a>
+    </div>
+</article>`
+    $(`#courses`).append(markup);
+    alert("Added successfully!");
+    $(".create-course-popup").hide();
 }
